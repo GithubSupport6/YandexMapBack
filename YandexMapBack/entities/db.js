e61d48eb11203df80d74f1288b80d25d4c9485e2
Grishka <gmelkof@ya.ru>
2019-11-27 23:53:37 +0300

    Добавьте файлы проекта.

---
+++ YandexMapBack/entities/db.js
@@ -0,0 +1,49 @@
+﻿const sqlite3 = require('sqlite3').verbose();
+
+class Db {
+    constructor(file) {
+        this.db = new sqlite3.Database(file);
+        this.createTable()
+    }
+
+    createTable() {
+        const sql = `
+            CREATE TABLE IF NOT EXISTS user (
+                id integer PRIMARY KEY, 
+                login text,
+                password text)`
+        return this.db.run(sql);
+    }
+
+    selectByLogin(email, callback) {
+        return this.db.get(
+            `SELECT * FROM user WHERE login = ?`,
+            [email], function (err, row) {
+                callback(err, row)
+            })
+    }
+
+    insertAdmin(user, callback) {
+        return this.db.run(
+            'INSERT INTO user (name,email,user_pass,is_admin) VALUES (?,?,?,?)',
+            user, (err) => {
+                callback(err)
+            })
+    }
+
+    selectAll(callback) {
+        return this.db.all(`SELECT * FROM user`, function (err, rows) {
+            callback(err, rows)
+        })
+    }
+
+    insert(user, callback) {
+        return this.db.run(
+            'INSERT INTO user (name,email,user_pass) VALUES (?,?,?)',
+            user, (err) => {
+                callback(err)
+            })
+    }
+}
+
+module.exports = Db 
---
+++ YandexMapBack/routes/login.js
@@ -0,0 +1,13 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+var db = require('./entities/db');
+
+/* GET home page. */
+router.post('/signin', function (req, res) {
+    db.
+});
+
+
+
+module.exports = router;
